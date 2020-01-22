ae1427f034fb4e492e8f190243f150fd85e1fbec
S-rim <deamoon0801@naver.com>
2020-01-23 01:02:35 +0900

    클래스 만들어보기

--- 자바클래스.java
+++ 자바클래스.java
@@ -6,6 +6,39 @@ class Main {
     }
   }
   
-  class Car {
+class Car {
   
+  }
+
+// 메소드 만들어 차가 달리게 하기
+
+  class Main {
+    public static void main(String[] args) {
+      Car car1 = new Car();
+      car1.run();
+    }
+  }
+  
+  class Car {
+    void run()
+    {
+      System.out.println("차가 달립니다.");
+    }
+  }
+
+// duck클래스 만들고 메소드 만들기
+
+  class Main {
+    public static void main(String[] args) {
+      duck aduck = new duck();
+      aduck.fly();
+    }
+  }
+  
+  class duck
+  {
+    void fly()
+    {
+      System.out.println("날았다.");
+    }
   }
